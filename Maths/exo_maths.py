c33a700645083ac08ba361de9ee52e22eec9e259
Guillaume Demerges <guillaume.demerges@protonmail.com>
2024-04-09 09:41:20 +0200

    update

--- Maths/exo_maths.py
+++ Maths/exo_maths.py
@@ -21,6 +21,23 @@ def surface_volume_area_cylinder(height, radius):
     surface_area = 2 * math.pi * radius * (radius + height)
     return volume, surface_area
 
+def surface_volume_area_sphere(radius_sphere):
+    surface_volume = (4/3) * math.pi * radius_sphere ** 3
+    area_sphere = 4 * math.pi * radius_sphere ** 2
+    return surface_volume, area_sphere
+
+def arc_length_angle(diameter_circle, angle_measure):
+    arc_lenght = (math.pi * diameter_circle) * (angle_measure / 360)
+    return arc_lenght
+
+def area_sector(radius_circle, angle_measure):
+    area = (math.pi * radius_circle ** 2) * (angle_measure / 360)
+    return area
+
+def discriminant_value(x, y, z):
+    discriminant = (y**2) - (4*x*z)
+    return discriminant
+
 degrees = 15
 radians = degrees_to_radians(degrees)
 print(f"{degrees} degrees is {radians} radians.")
@@ -39,3 +56,11 @@ print(f"Area is {area_trapezoid(5, 5, 6)}") #Autre façon d'afficher le résulta
 print(f"Area is {area_parallelogram(5, 6)}")
 
 print(f"Area is {surface_volume_area_cylinder(4, 6)}")
+
+print(f"Area is {surface_volume_area_sphere(0.75)}")
+
+print(f"Area is {arc_length_angle(8, 45)}")
+
+print(f"Area is {area_sector(4, 45)}")
+
+print(f"Area is {discriminant_value(4, 0, -4)}")
